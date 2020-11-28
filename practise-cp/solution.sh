0e25326592a86a2d8044d7683d29df79250bab68
qipengWei <webqipeng@163.com>
2020-11-28 22:18:35 +0800

    命令行CRUD

--- practise-cp/solution.sh
+++ practise-cp/solution.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env sh
-# 请在这里编写一条命令，将当前目录的src目录下所有的.java文件拷贝到dest目录
+# 请在这里编写一条命令，将当前目录的src目录下所有的.java文件拷贝到dest目录\
+
+cd src
+mv A.java ../dest/A.java
+mv B.java ../dest/B.java
+mv C.txt ../dest/C.txt
